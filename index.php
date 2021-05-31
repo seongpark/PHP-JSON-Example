bac48d0e25f799d9b28ad4b4edd6afbcef4a0f1e
oopseong <62174262+oopseong@users.noreply.github.com>
2021-05-31 17:40:37 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    $key = '3e206413f06d522942919b27f67ccad1d';
+    $key = 'shinekey';
     $data_str1 = file_get_contents('https://api.corona-19.kr/korea/?serviceKey='.$key);
     $json1 = json_decode($data_str1, true);
     echo 'NowCase: '. $json1['NowCase']. "<br>";
